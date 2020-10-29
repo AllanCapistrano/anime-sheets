083bdc4f78c9144379ac617e556b70701fd8a2ce
AllanCapistrano <allan.capistrano3@gmail.com>
2020-10-29 17:10:54 -0300

    Mostrando quantas atualizações faltam e porcentangem da atualização.[

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ SHEET_LINK = "https://docs.google.com/spreadsheets/d/1b5aRDVLz2a3zf4x5j0uJOeonr5
 
 crawler = Crawler()
 animes_url = sheets.getAnimeUrl()
+size = len(animes_url)
 
-for i in range(0, len(animes_url)):
+for i in range(0, size):
   last_episode = crawler.getLastEpisode(animes_url[i])
   last_episode_url = crawler.getLastEpisodeUrl(animes_url[i])
 
   sheets.setLastEpisode(i, last_episode)
   sheets.setLastEpisodeUrl(i, last_episode_url)
+
+  percentage = 100 * (i + 1)/size
+
+  print("Anime {}/{} atualizado | {:.2f}%".format(i + 1, size, percentage))
 
 print("\nPlanilha de animes atualizada com sucesso {}! \nLink: {}\n".format(USER, SHEET_LINK))
