a024e9e074e47260f7c96ee2fda7e4dce3319881
AllanCapistrano <allan.capistrano3@gmail.com>
2020-10-29 17:16:29 -0300

    Mostrando o tempo de execução.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from crawler import Crawler
 import sheets
+import time
 
 # --- Constants --- #
 USER = "Allan"
 SHEET_LINK = "https://docs.google.com/spreadsheets/d/1b5aRDVLz2a3zf4x5j0uJOeonr51Kmlu5vaIQ64b0wCY/edit#gid=0"
 # ---------------- #
+
+start = time.time()
 
 crawler = Crawler()
 animes_url = sheets.getAnimeUrl()
@@ -21,4 +24,7 @@ for i in range(0, size):
 
   print("Anime {}/{} atualizado | {:.2f}%".format(i + 1, size, percentage))
 
+end = time.time()
+
 print("\nPlanilha de animes atualizada com sucesso {}! \nLink: {}\n".format(USER, SHEET_LINK))
+print("Tempo de execução: {:.2f}s\n".format(end - start))
