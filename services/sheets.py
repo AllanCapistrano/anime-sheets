8708e2bde31152b72e01d9888582d7a478afe0d1
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-25 18:58:31 -0300

    Add documentation.

--- services/sheets.py
+++ services/sheets.py
@@ -1,14 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    'https://www.googleapis.com/auth/spreadsheets',
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-]
-
-# --- Constants --- #
+# ------------------------------ Constants ----------------------------------- #
 CREDS_FILE = "creds.json"
 SHEET_TITLE = "Animes"
 COL_URL = 3
@@ -16,7 +9,14 @@ COL_IM = 4
 COL_LE = 5
 COL_LEU = 6
 COL_LE_NAME = 'F'
-# ---------------- #
+# ---------------------------------------------------------------------------- #
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
 
 # Definindo as credenciais
 creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, scope)
@@ -27,37 +27,88 @@ client = gspread.authorize(creds)
 # Abrindo a planilha
 sheet = client.open(SHEET_TITLE).sheet1
 
-# Função que retorna uma lista das URL dos animes que estão na planilha.
-def getAnimeUrl():
-    anime_url = sheet.col_values(COL_URL)
-    anime_url.pop(0)
+def getAnimeUrl() -> list:
+    """ Função que retorna uma lista com as URL dos animes que estão na planilha.
 
-    return anime_url
+    Returns
+    -----------
+    animeUrl: :class:`list`
+    """
 
-# Função que retorna uma lista dos episódios que parei dos animes que estão na planilha.
-def getIm():
+    animeUrl = sheet.col_values(COL_URL)
+    animeUrl.pop(0)
+
+    return animeUrl
+
+def getIm() -> list:
+    """ Função que retorna uma lista com os episódios que parei dos animes que 
+    estão na planilha.
+
+    Returns
+    -----------
+    im: :class:`list`
+    """
+
     im = sheet.col_values(COL_IM)
     im.pop(0)
 
     return im
 
-# Função que retorna uma lista com o número dos últimos episódios que estão na planilha.
-def getLastEpisode():
-    last_episode = sheet.col_values(COL_LE)
-    last_episode.pop(0)
+# 
+def getLastEpisode() -> list:
+    """ Função que retorna uma lista com o número do último episódio lançado 
+    dos animes que estão na planilha.
 
-    return last_episode
+    Returns
+    -----------
+    lastEpisode: :class:`list`
+    """
+    
+    lastEpisode = sheet.col_values(COL_LE)
+    lastEpisode.pop(0)
 
-# Função que altera na planilha o número do último episódio lançado.
-def setLastEpisode(index, value):
+    return lastEpisode
+
+def setLastEpisode(index: int, value: str):
+    """ Função que altera na planilha o número do último episódio lançado.
+
+    Parameters
+    -----------
+
+    index: :class:`int`
+        Posição na planilha.
+    index: :class:`str`
+        Número do episódio.
+    """
+
     sheet.update_cell(index + 2, COL_LE, value)
 
-# Função que altera na planilha a URL do último episódio lançado.
-def setLastEpisodeUrl(index, value):
+def setLastEpisodeUrl(index: int , value: str):
+    """ Função que altera na planilha a URL do último episódio lançado.
+
+    Parameters
+    -----------
+
+    index: :class:`int`
+        Posição na planilha.
+    index: :class:`str`
+        Número do episódio.
+    """
+
     sheet.update_cell(index + 2, COL_LEU, value)
 
-# Função para trocar a cor de fundo da célula.
-def changeCellBackgroundColor(pos, color):
+def changeCellBackgroundColor(pos: int, color: list):
+    """ Função para alterar a cor de fundo de uma célula na planilha.
+
+    Parameters
+    -----------
+
+    pos: :class:`int`
+        Posição na planilha.
+    color: :class:`list`
+        Lista com os valores RGB ([red, green, blue]). 
+    """
+
     sheet.format(COL_LE_NAME + str(pos), {
         "backgroundColor": {
             "red": color[0],
