7f65be1ac3eb59327a7de7b28732275e958af48c
AllanCapistrano <allan.capistrano3@gmail.com>
2021-07-25 18:11:10 -0300

    Rename class and file.

---
+++ crawlerGoyabu.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+
+class CrawlerGoyabu:
+    def __reqUrl__(self, url: str) -> BeautifulSoup:
+        """ Função responsável por buscar as Urls.
+
+        Parameters
+        -----------
+
+        url: :class:`str`
+            Url do site.
+
+        Returns
+        -----------
+        soup: :class:`BeautifulSoup`
+        """
+
+        req  = requests.get(url)
+        soup = BeautifulSoup(req.text, 'lxml')
+
+        return soup
+
+    def getLastEpisode(self, url: str) -> str:
+        """ Função responsável por retornar o número do último episódio do anime.
+
+        Parameters
+        -----------
+
+        url: :class:`str`
+            Url do site.
+            
+        Returns
+        -----------
+        episodeNumber: :class:`str`
+        """
+
+        soup = self.__reqUrl__(url)
+
+        for animeInfo in soup.find_all("div", class_="anime-episode"):
+            for episodes in animeInfo.find_all("h3"):
+                episodeInfo = episodes.contents[0].split(" ")
+
+        for episodeNumber in episodeInfo:
+            if(episodeNumber.isnumeric()):
+                return episodeNumber
+
+    def getLastEpisodeUrl(self, url: str) -> str:
+        """ Função responsável por retornar a url do último episódio do anime.
+
+        Parameters
+        -----------
+
+        url: :class:`str`
+            Url do site.
+            
+        Returns
+        -----------
+        lastEpisodeUrl: :class:`str`
+        """
+
+        soup = self.__reqUrl__(url)
+
+        for animeInfo in soup.find_all("div", class_="anime-episode"):
+            for episodes in animeInfo.find_all("a"):
+                return episodes.attrs["href"]
