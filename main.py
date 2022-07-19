ec0364bd90a2c68bf5816f6d0e9983bd7b5dba1a
AllanCapistrano <allan.capistrano3@gmail.com>
2022-07-19 16:11:11 -0300

    Refactor: add main function

--- main.py
+++ main.py
@@ -16,81 +16,82 @@ USER_NAME  = getenv("USER_NAME")
 SHEET_LINK = getenv("SHEET_LINK")
 # ---------------------------------------------------------------------------- #
 
-console = Console()
-table   = Table()
-
-try:
-    sheet = Sheet()
-except Exception as error:
-    print()
-    console.print(error, style="bold red")
-    
-    exit()
-
-start = time()
-
-crawlerAnimesHouseAndAnimesOnline = CrawlerAnimesHouseAndAnimesOnline()
-crawlerGoyabu                     = CrawlerGoyabu()
-
-animeNames        = sheet.getAnimeNames()
-animeSeasons      = sheet.getAnimeSeasons()
-animesUrls        = sheet.getAnimeUrls()
-myEpisodes        = sheet.getMyEpisodes()
-lastEpisodesSheet = sheet.getLastEpisodes()
-animeBroadcasts   = sheet.getAnimeBroadcasts()
-
-lastEpisodesUpdated     = []
-lastEpisodesUrlsUpdated = []
-
-for i in track(range(0, len(animesUrls)), description="[cyan]Atualizando..."):
-    # Verifica qual é o site que está sendo utilizado para assistir o anime.
-    if(
-        animesUrls[i].find("animeshouse")  != -1 or 
-        animesUrls[i].find("animesonline") != -1
-    ):
-        lastEpisode    = crawlerAnimesHouseAndAnimesOnline.getLastEpisode(animesUrls[i])
-        lastEpisodeUrl = crawlerAnimesHouseAndAnimesOnline.getLastEpisodeUrl(animesUrls[i])
-    elif(animesUrls[i].find("goyabu") != -1):
-        lastEpisode    = crawlerGoyabu.getLastEpisode(animesUrls[i])
-        lastEpisodeUrl = crawlerGoyabu.getLastEpisodeUrl(animesUrls[i])
-
-    lastEpisodesUpdated.append(lastEpisode)
-    lastEpisodesUrlsUpdated.append(lastEpisodeUrl)
+if __name__ == "__main__":
+    console = Console()
+    table   = Table()
 
     try:
-        # Evita escritas desnecessárias.
-        if(lastEpisodesSheet[i] != lastEpisode):
-            sheet.setLastEpisode(i, lastEpisode)
-            sheet.setLastEpisodeUrl(i, lastEpisodeUrl)
-    except :
-        if(myEpisodes[i]):
-            sheet.setLastEpisode(i, lastEpisode)
-            sheet.setLastEpisodeUrl(i, lastEpisodeUrl)
-    
-    sheet.changeCellBackgroundColor(float(myEpisodes[i]), float(lastEpisode), i)
+        sheet = Sheet()
+    except Exception as error:
+        print()
+        console.print(error, style="bold red")
+        
+        exit()
 
-# Preenchendo a tabela.
-table.fillTable(
-    names            = animeNames, 
-    seasons          = animeSeasons, 
-    urls             = animesUrls, 
-    myEpisodes       = myEpisodes, 
-    lastEpisodes     = lastEpisodesUpdated, 
-    lastEpisodesUrls = lastEpisodesUrlsUpdated, 
-    broadcasts       = animeBroadcasts
-)
+    start = time()
 
-end = time()
+    crawlerAnimesHouseAndAnimesOnline = CrawlerAnimesHouseAndAnimesOnline()
+    crawlerGoyabu                     = CrawlerGoyabu()
 
-console.print("Tempo de execução: {:.2f}s\n".format(end - start), style="bold green")
+    animeNames        = sheet.getAnimeNames()
+    animeSeasons      = sheet.getAnimeSeasons()
+    animesUrls        = sheet.getAnimeUrls()
+    myEpisodes        = sheet.getMyEpisodes()
+    lastEpisodesSheet = sheet.getLastEpisodes()
+    animeBroadcasts   = sheet.getAnimeBroadcasts()
 
-if(USER_NAME != ""):
-    console.print("Planilha de animes atualizada com sucesso {}!".format(
-        USER_NAME), style="bold")
-if(SHEET_LINK != ""):
-    print("Link: {}".format(SHEET_LINK))
+    lastEpisodesUpdated     = []
+    lastEpisodesUrlsUpdated = []
 
-# Exibindo a tabela.
-table.showTable()
+    for i in track(range(0, len(animesUrls)), description="[cyan]Atualizando..."):
+        # Verifica qual é o site que está sendo utilizado para assistir o anime.
+        if(
+            animesUrls[i].find("animeshouse")  != -1 or 
+            animesUrls[i].find("animesonline") != -1
+        ):
+            lastEpisode    = crawlerAnimesHouseAndAnimesOnline.getLastEpisode(animesUrls[i])
+            lastEpisodeUrl = crawlerAnimesHouseAndAnimesOnline.getLastEpisodeUrl(animesUrls[i])
+        elif(animesUrls[i].find("goyabu") != -1):
+            lastEpisode    = crawlerGoyabu.getLastEpisode(animesUrls[i])
+            lastEpisodeUrl = crawlerGoyabu.getLastEpisodeUrl(animesUrls[i])
 
-print()
+        lastEpisodesUpdated.append(lastEpisode)
+        lastEpisodesUrlsUpdated.append(lastEpisodeUrl)
+
+        try:
+            # Evita escritas desnecessárias.
+            if(lastEpisodesSheet[i] != lastEpisode):
+                sheet.setLastEpisode(i, lastEpisode)
+                sheet.setLastEpisodeUrl(i, lastEpisodeUrl)
+        except :
+            if(myEpisodes[i]):
+                sheet.setLastEpisode(i, lastEpisode)
+                sheet.setLastEpisodeUrl(i, lastEpisodeUrl)
+        
+        sheet.changeCellBackgroundColor(float(myEpisodes[i]), float(lastEpisode), i)
+
+    # Preenchendo a tabela.
+    table.fillTable(
+        names            = animeNames, 
+        seasons          = animeSeasons, 
+        urls             = animesUrls, 
+        myEpisodes       = myEpisodes, 
+        lastEpisodes     = lastEpisodesUpdated, 
+        lastEpisodesUrls = lastEpisodesUrlsUpdated, 
+        broadcasts       = animeBroadcasts
+    )
+
+    end = time()
+
+    console.print("Tempo de execução: {:.2f}s\n".format(end - start), style="bold green")
+
+    if(USER_NAME != ""):
+        console.print("Planilha de animes atualizada com sucesso {}!".format(
+            USER_NAME), style="bold")
+    if(SHEET_LINK != ""):
+        print("Link: {}".format(SHEET_LINK))
+
+    # Exibindo a tabela.
+    table.showTable()
+
+    print()
