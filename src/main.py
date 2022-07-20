2359a7a8956606a1e1642f9ab9b3b07f2ef574c5
AllanCapistrano <allan.capistrano3@gmail.com>
2022-07-19 23:54:31 -0300

    Feat: using showOptions and add else to try-except

--- src/main.py
+++ src/main.py
@@ -3,80 +3,82 @@ from sys import exit
 from time import time
 from rich.console import Console
 
+from modules.showOptions import showOptions
 from modules.updateSheet import updateSheet
 from modules.showTable import showTable
 
 if __name__ == "__main__":
-    console = Console()
+    console       = Console()
+    clearTerminal = True
 
-    # TODO: Criar módulo de exibição de opções
-    system('cls' if name == 'nt' else 'clear')
-    console.print("[1] Exibir tabela", style="bold")
-    console.print("[2] Mais informações de um Anime", style="bold")
-    console.print("[3] Atualizar informações de um Anime", style="bold")
-    console.print("[4] Atualizar último episódio assistido", style="bold")
-    console.print("[5] Atualizar planilha", style="bold")
-    console.print("[6] Sair", style="bold")
+    while(True):
+        showOptions(clearTerminal=clearTerminal)
+        option = input("Escolha uma das opções acima: ")
 
-    option = input("Escolha uma das opções acima: ")
-
-    try:
-        option = int(option)
-    except:
-        raise ValueError("Erro! Opção inválida.")
-
-    match option:
-        case 1: # Exibir tabela
+        try:
+            option        = int(option)
+            clearTerminal = True
+        except:
             system('cls' if name == 'nt' else 'clear')
 
-            start = time()
-            updateSheet()
-            showTable()
-            end = time()
+            console.print("Opção inválida! Tente novamente.\n", style="bold red")
+            clearTerminal = False
+        else:
+            match option:
+                case 1: # Exibir tabela
+                    system('cls' if name == 'nt' else 'clear')
 
-            console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
-        case 2: # Mais informações de um Anime
-            system('cls' if name == 'nt' else 'clear')
+                    start = time()
+                    updateSheet()
+                    showTable()
+                    end = time()
 
-            start = time()
-            # TODO
-            end = time()
+                    console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
+                    input("\nPressione Enter para voltar...")
+                case 2: # Mais informações de um Anime
+                    system('cls' if name == 'nt' else 'clear')
 
-            console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
-            raise NotImplementedError("Método ainda não implementado")
-        case 3: # Atualizar informações de um Anime
-            system('cls' if name == 'nt' else 'clear')
+                    start = time()
+                    # TODO
+                    end = time()
 
-            start = time()
-            # TODO
-            end = time()
+                    console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
+                    input("\nPressione Enter para voltar...")
+                    raise NotImplementedError("Método ainda não implementado")
+                case 3: # Atualizar informações de um Anime
+                    system('cls' if name == 'nt' else 'clear')
 
-            console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
-            raise NotImplementedError("Método ainda não implementado")
-        case 4: # Atualizar último episódio assistido
-            system('cls' if name == 'nt' else 'clear')
+                    start = time()
+                    # TODO
+                    end = time()
 
-            start = time()
-            # TODO
-            end = time()
+                    console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
+                    input("\nPressione Enter para voltar...")
+                    raise NotImplementedError("Método ainda não implementado")
+                case 4: # Atualizar último episódio assistido
+                    system('cls' if name == 'nt' else 'clear')
 
-            console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
-            raise NotImplementedError("Método ainda não implementado")
-        case 5: # Atualizar planilha
-            system('cls' if name == 'nt' else 'clear')
+                    start = time()
+                    # TODO
+                    end = time()
 
-            start = time()
-            updateSheet(showLog=True)
-            end = time()
+                    console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
+                    input("\nPressione Enter para voltar...")
+                    raise NotImplementedError("Método ainda não implementado")
+                case 5: # Atualizar planilha
+                    system('cls' if name == 'nt' else 'clear')
 
-            console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
-        case 6:
-            system('cls' if name == 'nt' else 'clear')
-            
-            console.print("Até logo!", style="bold")
-            exit()
-        
-        case _:
-            console.print("Opção inválida! Tente novamente.", style="bold red")
+                    start = time()
+                    updateSheet(showLog=True)
+                    end = time()
 
-            exit() # Temporário
+                    console.print("Tempo de execução: {:.2f}s".format(end - start), style="bold green")
+                    input("\nPressione Enter para voltar...")
+                case 6:
+                    console.print("\nAté logo!", style="bold")
+                    exit()
+                case _:
+                    system('cls' if name == 'nt' else 'clear')
+
+                    console.print("Opção inválida! Tente novamente.\n", style="bold red")
+                    clearTerminal = False
