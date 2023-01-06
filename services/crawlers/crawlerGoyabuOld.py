5310876a28af27ac4f01c093d5f98127b9d65aab
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-05 22:28:44 -0300

    Refactor: update getLastEpisode and getLastEpisodeUrl methods

--- services/crawlers/crawlerGoyabuOld.py
+++ services/crawlers/crawlerGoyabuOld.py
@@ -18,6 +18,50 @@ class CrawlerGoyabuOld(Crawler, CrawlerInterface):
 
         soup = self.reqUrl(url)
 
+        animeInfo = soup.find("div", class_="anime-episode")
+        episode = animeInfo.find("h3")
+        episodeInfo = episode.contents[0].split(" ")
+
+        for episodeNumber in episodeInfo:
+            if(episodeNumber.isnumeric()):
+                return episodeNumber
+
+    def getLastEpisodeUrl(self, url: str) -> str:
+        """ Função responsável por retornar a url do último episódio do anime.
+
+        Parameters
+        -----------
+        url: :class:`str`
+            Url do site.
+            
+        Returns
+        -----------
+        lastEpisodeUrl: :class:`str`
+        """
+
+        soup = self.reqUrl(url)
+        animeInfo = soup.find("div", class_="anime-episode")
+        episode = animeInfo.find("a")
+        lastEpisodeUrl = episode.attrs["href"]
+
+        return lastEpisodeUrl
+
+    def getLastEpisodeOld(self, url: str) -> str:
+        """ Função responsável por retornar o número do último episódio do 
+        anime.
+
+        Parameters
+        -----------
+        url: :class:`str`
+            Url do site.
+            
+        Returns
+        -----------
+        episodeNumber: :class:`str`
+        """
+
+        soup = self.reqUrl(url)
+
         for animeInfo in soup.find_all("div", class_="anime-episode"):
             for episodes in animeInfo.find_all("h3"):
                 episodeInfo = episodes.contents[0].split(" ")
@@ -26,7 +70,7 @@ class CrawlerGoyabuOld(Crawler, CrawlerInterface):
             if(episodeNumber.isnumeric()):
                 return episodeNumber
 
-    def getLastEpisodeUrl(self, url: str) -> str:
+    def getLastEpisodeUrlOld(self, url: str) -> str:
         """ Função responsável por retornar a url do último episódio do anime.
 
         Parameters
