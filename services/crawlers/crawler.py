87073a06f506ba55ad6feb29ab3ec390b1efc8fc
AllanCapistrano <allan.capistrano3@gmail.com>
2023-12-27 11:09:22 -0300

    feat: create reqWebPage method

--- services/crawlers/crawler.py
+++ services/crawlers/crawler.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+from ..webpage import getWebPage, deleteWebpage
+
+# ------------------------------ Constants ----------------------------------- #
+DIRECTORY_PATH = "web"
+# ---------------------------------------------------------------------------- #
+
 class Crawler:
     def reqUrl(self, url: str) -> BeautifulSoup:
         """ Função responsável por buscar as Urls.
@@ -17,5 +23,32 @@ class Crawler:
 
         req  = requests.get(url)
         soup = BeautifulSoup(req.text, 'lxml')
+
+        return soup
+    
+    def reqWebPage(self, url: str, webpage_name: str = "index.html") -> BeautifulSoup:
+        """ Função responsável por buscar as Urls e fazer download das páginas.
+
+        Parameters
+        -----------
+        url: :class:`str`
+            Url do site.
+        webpage_name: :class:`str`
+            Nome do arquivo. Por padrão é `index.html`.
+
+        Returns
+        -----------
+        soup: :class:`BeautifulSoup`
+        """
+        
+        webpage_name = f"{DIRECTORY_PATH}/{webpage_name}"
+        
+        getWebPage(url=url)
+
+        file = open(webpage_name)
+        soup = BeautifulSoup(file, 'html.parser')
+        file.close
+
+        deleteWebpage()
 
         return soup
