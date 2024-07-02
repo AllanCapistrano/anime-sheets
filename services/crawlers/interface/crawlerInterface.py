0996e9ef19535f8f481a2590ffa11fa0edc912e0
AllanCapistrano <allan.capistrano3@gmail.com>
2024-07-02 11:14:23 -0300

    docs: improving interface docs

--- services/crawlers/interface/crawlerInterface.py
+++ services/crawlers/interface/crawlerInterface.py
@@ -1,35 +1,28 @@
 from abc import ABC, abstractmethod
 
 class CrawlerInterface(ABC):
+    """Interface para os crawlers
+    """
+
     @abstractmethod
     def getLastEpisode(self, url: str) -> str:
-        """ Função responsável por retornar o número do último episódio do 
+        """Função responsável por retornar o número do último episódio do
         anime.
 
-        Parameters
-        -----------
-        url: :class:`str`
-            Url do site.
-            
-        Returns
-        -----------
-        episodeNumber: :class:`str`
-        """
+        Args:
+            url (str): Url do site.
 
-        pass
+        Returns:
+            str
+        """
 
     @abstractmethod
     def getLastEpisodeUrl(self, url: str) -> str:
-        """ Função responsável por retornar a url do último episódio do anime.
+        """Função responsável por retornar a url do último episódio do anime.
 
-        Parameters
-        -----------
-        url: :class:`str`
-            Url do site.
-            
-        Returns
-        -----------
-        lastEpisodeUrl: :class:`str`
+        Args:
+            url (str): Url do site.
+
+        Returns:
+            str
         """
-
-        pass
